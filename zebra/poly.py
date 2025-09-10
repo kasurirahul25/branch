31aff650b0587cc45ad4f0218d3a07341cc00d97
Rahul <kasurirahul6@gmail.com>
2025-09-10 17:23:23 +0530

    3

---
+++ zebra/poly.py
@@ -0,0 +1,8 @@
+
+ymorpshim   
+# -*- coding: utf-8 -*-
+"""
+This module provides polynomial operations including addition, subtraction,
+multiplication, division, and evaluation. It also includes a function to find
+the roots of a polynomial using NumPy.  
+"""
